b187751a8bc73abf480194046f83db28fd59edbc
zengli <zzlili@cn.ibm.com>
2019-11-22 09:21:36 +0800

    Add transaction local time.

--- src/main/java/com/mentorondemand/paymentservice/PaymentApplication.java
+++ src/main/java/com/mentorondemand/paymentservice/PaymentApplication.java
@@ -1,5 +1,9 @@
 package com.mentorondemand.paymentservice;
 
+import java.util.TimeZone;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
@@ -7,6 +11,11 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @EnableEurekaClient
 @SpringBootApplication
 public class PaymentApplication {
+
+	@PostConstruct
+	public void init() {
+		TimeZone.setDefault(TimeZone.getTimeZone("CST"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(PaymentApplication.class, args);
